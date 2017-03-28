d662c134ed1e0db9a8dcd73feee53b6b8fd0bab1
Vladimir Grigore <vladimir.grigore@gmail.com>
2017-03-28 23:21:32 +0000

    Create first db migrate

--- knexfile.js
+++ knexfile.js
@@ -6,45 +6,16 @@ module.exports = {
   development: {
     client: 'pg',
     connection: {
-      user     : 'settings.user',
-      password : 'settings.password',
-      database : 'settings.database',
-      host     : 'settings.hostname',
-      port     : 'settings.port',
-      ssl      : 'settings.ssl'
+      user     : settings.user,
+      password : settings.password,
+      database : settings.database,
+      host     : settings.hostname,
+      port     : settings.port,
+      ssl      : settings.ssl
     }
-  },
+  },  
 
-  staging: {
-    client: 'pg',
-    connection: {
-      database: 'settings.database',
-      user:     'settings.username',
-      password: 'settings.password'
-    },
-    pool: {
-      min: 2,
-      max: 10
-    },
-    migrations: {
-      tableName: 'knex_migrations'
-    }
-  },
-
-  production: {
-    client: 'pg',
-    connection: {
-      database: 'settings.database',
-      user:     'settings.username',
-      password: 'settings.password'
-    },
-    pool: {
-      min: 2,
-      max: 10
-    },
-    migrations: {
-      tableName: 'knex_migrations'
-    }
+  migrations: {
+    tableName: 'knex_migrations'
   }
-
 };
