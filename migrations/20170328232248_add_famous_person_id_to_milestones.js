7d965bc1826c873ada9623871273b456055b6e78
Vladimir Grigore <vladimir.grigore@gmail.com>
2017-03-29 00:18:21 +0000

    Create second db migration

---
+++ migrations/20170328232248_add_famous_person_id_to_milestones.js
@@ -0,0 +1,13 @@
+exports.up = function(knex, Promise) { 
+  return knex.schema.table("milestones", (table) => {
+    table.integer("famous_person_id");
+    table.foreign("famous_person_id").references("id").inTable("famous_people");
+  })
+};
+
+exports.down = function(knex, Promise) {
+  return knex.schema.table("milestones", (table) => {
+    table.dropForeign("famous_person_id");
+    table.dropColumn("famous_person_id");
+  })
+};
